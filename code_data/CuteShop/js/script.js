e76ca2add4c3c8343bafd860cbb7f52a0e57b86e
maivinhlam <maivinhlam910@gmail.com>
2020-08-07 00:07:55 +0700

    Set size for MegaMenu form size of search bar, use js

--- code_data/CuteShop/js/script.js
+++ code_data/CuteShop/js/script.js
@@ -5,3 +5,14 @@ $(function () {
     $('.offcanvas-collapse').toggleClass('open')
   })
 })
+setWidthForMegaMenu();
+function setWidthForMegaMenu() {
+  var root = document.getElementById('header-sidebar-content').getElementsByClassName('megamenu-content');
+  var sizeformsearch = document.getElementsByClassName('formsearch')[0].clientWidth;
+
+  for (let index = 0; index < root.length; index++) {
+    root[index].style.width = sizeformsearch + 'px';
+
+  }
+
+}
