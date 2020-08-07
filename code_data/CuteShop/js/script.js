3bfb3e9b4ffdd7af8a2d85ce663e595182d5d55c
maivinhlam <maivinhlam910@gmail.com>
2020-08-07 11:25:27 +0700

    Show side bar when small device

--- code_data/CuteShop/js/script.js
+++ code_data/CuteShop/js/script.js
@@ -6,13 +6,25 @@ $(function () {
   })
 })
 setWidthForMegaMenu();
+window.addEventListener("resize", setWidthForMegaMenu);
 function setWidthForMegaMenu() {
   var root = document.getElementById('header-sidebar-content').getElementsByClassName('megamenu-content');
   var sizeformsearch = document.getElementsByClassName('formsearch')[0].clientWidth;
 
   for (let index = 0; index < root.length; index++) {
     root[index].style.width = sizeformsearch + 'px';
-
   }
 
+}
+
+function showsidebar() {
+  if (window.innerWidth < 992) {
+    var root = document.getElementById("header-sidebar-content");
+    var classname = root.className;
+    if (classname.search('d-none') > 0) {
+      root.classList.remove('d-none');
+    } else {
+      root.className += ' d-none';
+    }
+  }
 }
